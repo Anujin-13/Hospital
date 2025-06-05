6cebe67afad1990eacb9b933cccd8050a1566ef3
Таны нэр < >
2025-06-05 10:51:33 +0800

    appointment

---
+++ Appointment.java
@@ -0,0 +1,15 @@
+public class Appointment {
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+
+    public Appointment(Patient patient, Doctor doctor, String date) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    public String toString() {
+        return patient + "\nЦаг захиалсан: " + date + "\n" + doctor;
+    }
+}
